156b734727ccaa797f27c9e97f17d91706cb3749
jfaucette <javonifaucette@hotmail.com>
2015-10-23 14:26:31 -0400

    Finished tutorial

---
+++ app/src/main/java/com/example/jfaucette/helloworld/MyService.java
@@ -0,0 +1,30 @@
+package com.example.jfaucette.helloworld;
+
+/**
+ * Created by JFaucette on 10/23/15.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
